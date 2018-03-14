17af05bed1670f51eddf4cfae91f1c93f4b2f13d
Jas <jsingh01@manhattan.edu>
2018-03-14 19:39:34 -0400

    Added directional flags

--- SensorRead/usonic.py
+++ SensorRead/usonic.py
@@ -1,4 +1,18 @@
 #/usr/bin/env python
+def avoid(flagFront,flagRear,flagLeft,flagRight,):
+    if (!flagFront && !flagRear && !flagRight && !flagLeft):
+        print("Normal Flight")
+    else if (flagFront && !flagRear && !flagRight && !flagLeft):
+        print("Object in front")
+    else if (!flagFront && flagRear && !flagRight && !flagLeft):
+        print("Object in rear")
+    else if (!flagFront && !flagRear && flagRight && !flagLeft):
+        print("Object on right")
+    else if (!flagFront && !flagRear && !flagRight && flagLeft):
+        print("Object on Left")
+    else:
+        print("Your surrounded")
+
 if __name__ == "__main__":
 
    import time
@@ -10,29 +24,45 @@ if __name__ == "__main__":
    if not pi.connected:
       exit()
 
-   S=[]
-   Readings = [0,0,0,0,0,0,0,0]
-   S.append(srte.sonar(pi, 17, 27))
-   S.append(srte.sonar(pi, 23, 24))
+   S=[] #this will hold sensor objects
+   Readings = [0,0,0,0,0,0,0,0] #the most recent reading values from the sensor
+   S.append(srte.sonar(pi, 17, 27)) #sensor trigger pin 17/ echo pin 27
+   S.append(srte.sonar(pi, 23, 24)) #sensor trigger pin 23/ echo pin 24
    end = time.time() + 1.0
+   flagFront = False
+   flagLeft = False
+   flagRight = False
+   flagRear = False
 
    r = 1
 
    try:
       while time.time() < end:
 
-         for s in S:
+        for s in S: #iterate through each sensor
             s.trigger()
 
-         time.sleep(0.02)
+        time.sleep(0.02)
 
-         for s in range(len(S)):
+        for s in range(len(S)):#read each sensor and store in readings array
             Readings[s] = S[s].read()
+            if (readings[s] <= 50):
+                if (s == 0 or s == 1):
+                    flagFront = True
+                if (s == 2 or s == 3):
+                    flagRight = True
+                if (s == 4 or s == 5):
+                    flagRear = True
+                if (s == 6 or s == 7):
+                    flagFront = True
 	    print("{} {:.1f}".format(r, Readings[s]))
 
-         time.sleep(0.02)
+        time.sleep(0.01)
+        avoid(flagFront,flagRear,flagLeft,flagRight)
 
-         r += 1
+
+        r += 1
+
 
    except KeyboardInterrupt:
       pass
@@ -41,5 +71,5 @@ if __name__ == "__main__":
 
    for s in S:
       s.cancel()
-   print (Readings)
+   print (Readings) #print readings
    pi.stop()
