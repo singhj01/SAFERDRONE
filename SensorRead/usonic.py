0f681347b48c18f4d5c79c9fb1530254d55f8099
Jaspreet <jsingh01@manhattan.edu>
2018-03-15 19:49:55 -0400

    fastest approach

--- SensorRead/usonic.py
+++ SensorRead/usonic.py
@@ -1,26 +1,27 @@
 #/usr/bin/env python
+def Normal():
+    test = 1;
+   # print ("Normal Flight")
+def Front():
+    test = 1;
+   # print ("Object to the front")
+def Left():
+    test = 1;
+   # print ("Object to the left")
+def Rear():
+    test = 1;
+   # print ("Object to the rear")
+def Right():
+    test = 1;
+   # print ("object to the right")
 
-def avoid(flagFront,flagRear,flagLeft,flagRight):
-    if not flagFront and not flagRear and not flagRight and not flagLeft:
-        test = True
-        #print("Normal Flight")
-    elif flagFront and not flagRear and not flagRight and not flagLeft:
-        test = True
-        #print("Object in front")
-    elif not flagFront and flagRear and not flagRight and not flagLeft:
-        test = True
-        #print("Object in rear")
-    elif not flagFront and not flagRear and flagRight and not flagLeft:
-        test = True
-        #print("Object on right")
-    elif not flagFront and not flagRear and not flagRight and flagLeft:
-        test = True
-        #print("Object on Left")
-    else:
-        test = True
-        #print("You're surrounded")
-
-        
+directionFlags = {0:Normal,
+			1:Front,
+			2:Left,
+			4:Rear,
+			8:Right,
+}
+      
 if __name__ == "__main__":
 
    import time
@@ -37,17 +38,17 @@ if __name__ == "__main__":
    S.append(srte.sonar(pi, 17, 27)) #sensor trigger pin 17/ echo pin 27
    S.append(srte.sonar(pi, 23, 24)) #sensor trigger pin 23/ echo pin 24
    end = time.time() + 30.0
+  
 
 
    r = 1
 
    try:
         while time.time() < end:
-            #directional flags to determine which direction to avoid
-            flagFront = False
-            flagLeft = False
-            flagRight = False
-            flagRear = False
+            #directional flags to determine which direction to avoid 
+	    #bits are defined as right,rear,left,front where a 1 in that bit
+	    # means an object is present that direction
+            direction = 0
 
             for s in S: #iterate through each sensor
                 s.trigger()
@@ -57,22 +58,18 @@ if __name__ == "__main__":
             for s in range(len(S)):#read each sensor and store in readings array
                 Readings[s] = S[s].read()
                 if (Readings[s] <= 100):
-                    if (s == 0 or s == 1):
-                        flagFront = True
-                    if (s == 2 or s == 3):
-                        flagRight = True
-                    if (s == 4 or s == 5):
-                        flagRear = True
-                    if (s == 6 or s == 7):
-                        flagFront = True
+                    if (s == 0 or s == 1):#front sensors
+                        direction = direction | 1
+                    if (s == 2 or s == 3):#left sensors
+                        direction = direction | 2
+                    if (s == 4 or s == 5):#rear sensors
+                        direction = direction | 4
+                    if (s == 6 or s == 7):#right sensors
+                        direction = direction | 8
                 #print("{} {:.1f}".format(r, Readings[s]))
-                print r
-            avoid(flagFront,flagRear,flagLeft,flagRight)
-            time.sleep(0.01)
-
-            
-
-
+                #print r
+            directionFlags[direction]()
+            time.sleep(0.005)
 
             r += 1
 
@@ -84,5 +81,6 @@ if __name__ == "__main__":
 
    for s in S:
       s.cancel()
-   print (Readings) #print readings
+   print Readings #print readings
+   print r
    pi.stop()
