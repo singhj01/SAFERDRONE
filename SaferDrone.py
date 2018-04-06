7bfddbe4f1b0bd659a7ee5ed0dfe912712172ce9
singhj01 <Jsingh01@manhattan.edu>
2018-04-06 19:34:16 -0400

    idk

--- SaferDrone.py
+++ SaferDrone.py
@@ -111,7 +111,6 @@ if __name__ == "__main__":
 
    S=[[4,17],[27,22],[05,06],[13,19],[26,21],[16,20],[25,12],[23,24]] #this will hold sensor GPIO channel numbers {trig/echo,}
    Readings = [0,0,0,0,0,0,0,0] #the most recent reading values from the sensor
-   #end = time.time() + 900.0
    LIMIT = 30 #minimum distance
 
    r = 1
@@ -134,11 +133,11 @@ if __name__ == "__main__":
                    start_time = time.time()
 		while (GPIO.input(S[s][1]) == 1):                
                    stop_time = time.time()
-                   if (stop_time - start_time >= .018):
+                   if (stop_time - start_time >= .018): #timeout after 18ms
                       break
                    
 
-                Readings[s] = (stop_time-start_time)*34029/2
+                Readings[s] = (stop_time-start_time)*34029/2 #reading in cm
                 if (Readings[s] <= LIMIT):
                     if (s == 0):#front sensors
                         direction = direction | 1 #0001
